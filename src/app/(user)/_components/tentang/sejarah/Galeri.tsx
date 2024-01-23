8c5df477722fcc2d4da2ba6fc171eb5af2f2ea99
PrastowoAdi <sinaadi75@gmail.com>
2024-01-24 01:06:32 +0700

    add galeri

--- src/app/(user)/_components/tentang/sejarah/Galeri.tsx
+++ src/app/(user)/_components/tentang/sejarah/Galeri.tsx
@@ -1,21 +1,36 @@
+"use client";
+
 import Image from "next/image";
 
 import { TENTANG_GEREJA } from "@/configs";
+import { useCardGaleri } from "@/hooks/use-card-galeri";
 
 export default function Galeri() {
+  const galeriModal = useCardGaleri();
   return (
-    <div className="grid grid-cols-2 gap-2 pt-14 md:grid-cols-3 lg:gap-5">
+    <div className="grid grid-cols-3 gap-4 pt-14 md:grid-cols-6 lg:gap-5">
       {TENTANG_GEREJA.galeri.map((item, idx) => (
         <div
-          className="relative aspect-square overflow-hidden rounded shadow-lg"
+          className="relative h-40 cursor-pointer overflow-hidden rounded shadow-lg transition-all duration-300 hover:scale-105 md:h-64"
           key={idx}
+          onClick={() =>
+            galeriModal.onOpen(
+              TENTANG_GEREJA.galeri.map((e, idx) => {
+                return {
+                  id: idx + 1,
+                  title: e,
+                };
+              }),
+              idx + 1,
+            )
+          }
         >
           <Image
             className="object-cover"
             src={item}
             alt={`img-${idx + 1}`}
             fill
-            sizes="(min-width: 1360px) 360px, (min-width: 780px) calc(29.82vw - 40px), calc(100vw - 20px)"
+            sizes="(min-width: 1340px) 170px, (min-width: 780px) calc(14.81vw - 26px), calc(50vw - 18px)"
           />
         </div>
       ))}
