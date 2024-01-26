52da595b67df27c2475fc52b866cb1d1a01f0964
PrastowoAdi <sinaadi75@gmail.com>
2024-01-26 17:38:08 +0700

    add toggle dark mode in mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -2,11 +2,14 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
-import { Menu, X } from "lucide-react";
+import { Menu, Moon, Sun, X } from "lucide-react";
 
 import Link from "next/link";
 import Image from "./Image";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+import { ModeToggle } from "./DarkModeToggle";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
 
 type NavMenuType = {
   label: string;
@@ -25,6 +28,7 @@ interface MobileNavProps {
 }
 
 const MobileNav = ({ navmenu }: MobileNavProps) => {
+  const { setTheme } = useTheme();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const navRef = useRef<HTMLDivElement | null>(null);
 
@@ -56,23 +60,39 @@ const MobileNav = ({ navmenu }: MobileNavProps) => {
   return (
     <div>
       <div className="relative z-40 lg:hidden">
-        <div className="fixed inset-0 bg-black bg-opacity-25" />
+        <div className="fixed inset-0 bg-black bg-opacity-25 dark:bg-white dark:bg-opacity-25" />
       </div>
 
       <div className="fixed inset-0 z-40 flex overflow-y-scroll overscroll-y-none">
         <div className="w-4/5">
           <div
-            className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white/90 pb-12 shadow-xl backdrop-blur-md"
+            className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white/90 pb-12 shadow-xl backdrop-blur-md dark:bg-background"
             ref={navRef}
           >
-            <div className="flex px-4 pb-2 pt-5">
+            <div className="flex items-center justify-between px-4 pb-2 pt-5">
               <button
                 type="button"
                 onClick={() => setIsOpen(false)}
-                className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-black"
+                className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-black dark:text-white"
               >
                 <X className="h-6 w-6" aria-hidden="true" />
               </button>
+              <div className="flex gap-2">
+                <Button
+                  size="icon"
+                  variant="secondary"
+                  onClick={() => setTheme("light")}
+                >
+                  <Sun />
+                </Button>
+                <Button
+                  size="icon"
+                  variant="secondary"
+                  onClick={() => setTheme("dark")}
+                >
+                  <Moon />
+                </Button>
+              </div>
             </div>
 
             <div className="mt-2">
@@ -81,7 +101,7 @@ const MobileNav = ({ navmenu }: MobileNavProps) => {
                   <li key={item.label} className="space-y-10 px-4 pb-8 pt-10">
                     <div className="border-b border-gray-400">
                       <div className="-mb-px flex">
-                        <p className="flex-1 whitespace-nowrap border-b-2 border-transparent py-4 text-lg font-medium text-gray-900">
+                        <p className="flex-1 whitespace-nowrap border-b-2 border-transparent py-4 text-lg font-medium text-gray-900 dark:text-white">
                           {item.label}
                         </p>
                       </div>
@@ -93,7 +113,7 @@ const MobileNav = ({ navmenu }: MobileNavProps) => {
                           key={item.name}
                           className="group relative text-base"
                         >
-                          <div className="relative h-3/4 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                          <div className="relative h-3/4 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75 dark:bg-background">
                             <Image
                               width={400}
                               height={300}
@@ -104,7 +124,7 @@ const MobileNav = ({ navmenu }: MobileNavProps) => {
                           </div>
                           <Link
                             href={item.href}
-                            className="mt-2 block text-sm font-medium text-gray-700"
+                            className="mt-2 block text-sm font-medium text-gray-700 dark:text-gray-300"
                           >
                             {item.name}
                           </Link>
